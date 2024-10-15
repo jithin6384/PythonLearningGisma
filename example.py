9c1861d585cb19615393a14114bf5bc82a77ffc3
Jithin R <raghunathan.jithin@gmail.com>
2024-10-15 14:01:51 +0200

    initial commit

---
+++ example.py
@@ -0,0 +1,9 @@
+print("hello world");
+
+user_name = input("Whats your Name:");
+
+
+print(f'Your name is {user_name}');
+
+print("Your name is {name}".format(name=user_name));
+
---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string user_name;
+    cout << "Write Your name \n";
+    cin >> user_name;
+    cout << "Your name is " << user_name;
+    return 0;
+}
